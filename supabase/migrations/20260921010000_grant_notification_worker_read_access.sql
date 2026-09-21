b6c3daa2c8507a408b60404db7d0d2c80059b241
GSBC24 <discosgsbc@gmail.com>
2026-09-21 23:45:49 +0200

    Grant notification worker database access

---
+++ supabase/migrations/20260921010000_grant_notification_worker_read_access.sql
@@ -0,0 +1,7 @@
+GRANT SELECT ON public.deals TO service_role;
+GRANT SELECT ON public.deal_schedules TO service_role;
+GRANT SELECT ON public.favorites TO service_role;
+GRANT SELECT ON public.notification_preferences TO service_role;
+GRANT SELECT ON public.notification_devices TO service_role;
+GRANT SELECT ON public.businesses TO service_role;
+GRANT DELETE ON public.notification_devices TO service_role;
